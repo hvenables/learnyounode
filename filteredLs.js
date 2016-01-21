72b221c3b5b6e6406e62db2bab8b7c5fa92c2d77
Harry Venables <venablesh@yahoo.co.uk>
2016-01-21 02:22:55 +0000

    refactored filtered ls again

--- filteredLs.js
+++ filteredLs.js
@@ -1,10 +1,11 @@
 var fs = require('fs');
 var path = require('path');
 var pathName = process.argv[2];
+var extension = process.argv[3];
 
 fs.readdir(pathName, function(err, list) {
   for(i = 0; i < list.length; i++) {
-    if (path.extname(list[i]) === "." + process.argv[3]) {
+    if (path.extname(list[i]) === "." + extension) {
       console.log(list[i]);
     }
   }
